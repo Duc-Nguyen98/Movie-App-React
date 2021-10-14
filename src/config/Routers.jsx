a2745ed6e967526c6a9c123d5378fb69d3815c49
Nguyễn Quang Đức <ducnin1998@gmail.com>
2021-10-15 03:53:04 +0700

    Update Routers.jsx

--- src/config/Routers.jsx
+++ src/config/Routers.jsx
@@ -12,7 +12,7 @@ const Routers = () => {
   return (
     <Switch>
       <Route path="/:category/search/:keyword" component={Catalog} />
-      <Route path="/:category/id" component={Detail} />
+      <Route path="/:category/:id" component={Detail} />
       <Route path="/:category" component={Catalog} />
       <Route path="/" exact component={Home} />
     </Switch>
