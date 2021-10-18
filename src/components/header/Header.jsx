349c5a0019dc98af9f2fcef2dbeb37381faa17ab
Nguyễn Quang Đức <ducnin1998@gmail.com>
2021-10-18 12:36:22 +0700

    Config 4

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,7 +6,7 @@ import React, { useRef, useEffect } from "react";
 import "./header.scss";
 import LogoText from "../../assets/logo-text.png";
 
-import { Link, useLocation } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 
 const headerNav = [
   {
@@ -31,16 +31,19 @@ const Header = () => {
   //   console.log(`this ${active}`)
 
   useEffect(() => {
-    const shrinkHeader = () =>{
-        if(document.body.scrollTop > 100 || document.documentElement.scrollTop > 100){
-            headerRef.current.classList.add('shrink');   // add css 
-        }else{
-            headerRef.current.classList.remove('shrink'); //remove css
-        }
-    }
-    window.addEventListener('scroll', shrinkHeader) // window add Event call function  shrinkHeader
+    const shrinkHeader = () => {
+      if (
+        document.body.scrollTop > 100 ||
+        document.documentElement.scrollTop > 100
+      ) {
+        headerRef.current.classList.add("shrink"); // add css
+      } else {
+        headerRef.current.classList.remove("shrink"); //remove css
+      }
+    };
+    window.addEventListener("scroll", shrinkHeader); // window add Event call function  shrinkHeader
     return () => {
-        window.removeEventListener('scroll', shrinkHeader) // window remove Event call function  shrinkHeader
+      window.removeEventListener("scroll", shrinkHeader); // window remove Event call function  shrinkHeader
     };
   }, []);
 
@@ -48,13 +51,15 @@ const Header = () => {
     <div ref={headerRef} className="header">
       <div className="header__wrap container">
         <div className="logo">
-          <img src={LogoText} alt="BeeMovies-Logo" />
+          <NavLink to="/">
+            <img src={LogoText} alt="BeeMovies-Logo" />{" "}
+          </NavLink>
           {/* <Link to="/">tMovies</Link> */}
         </div>
         <ul className="header__nav">
           {headerNav.map((e, i) => (
             <li key={i} className={`${i === active ? "active" : ""} `}>
-              <Link to={e.path}>{e.display}</Link>
+              <NavLink to={e.path}>{e.display}</NavLink>
             </li>
           ))}
         </ul>
